b2dc990fb041e170f4d8b9aa7963f7a3e4dff60f
Vishal Taral <vishaltaral09@gmail.com>
2023-12-05 16:13:43 +0530

    header

--- apps/goals-on-track/component/common/layout/layout.tsx
+++ apps/goals-on-track/component/common/layout/layout.tsx
@@ -1,12 +1,16 @@
 import styles from './layout.module.scss';
+import {Header} from '../../../../../libs/shared/ui/src/lib/components/header/header';
 
 /* eslint-disable-next-line */
-export interface LayoutProps {}
+export interface LayoutProps {
+  children : any;
+}
 
 export function Layout(props: LayoutProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Layout!</h1>
+    <div className={styles.container}>
+      <Header />
+      <div>{props.children}</div>
     </div>
   );
 }
--- apps/goals-on-track/pages/index.tsx
+++ apps/goals-on-track/pages/index.tsx
@@ -1,10 +1,13 @@
 import styles from './index.module.scss';
+import Layout from '../component/common/layout/layout';
 
 export function Index() {
 
   return (
     <div className={styles.page}>
-      xxx
+      <Layout>
+        <h1>Welcome to goal tracking app</h1>
+      </Layout>
     </div>
   );
 }
