639d7a0b7b5c37c41297b8f388fb2eeeaa855864
atharvabhuse <atharva.bhuse@polyglots.io>
2023-12-18 12:17:09 +0530

    get api call for categories and roles

--- libs/shared/data-access/src/lib/api-end-points.ts
+++ libs/shared/data-access/src/lib/api-end-points.ts
@@ -1,12 +1,12 @@
 const serverUrl = 'http://192.168.1.104:3333/api';
 
 export const apiUrlObject = {
-    allCategoriesApiUrl : `${serverUrl}/categories`,
-    getAllRoles : `${serverUrl}/roles`,
+    // getAllCategories : `${serverUrl}/categories`,
+    // getAllRoles : `${serverUrl}/roles`,
     updateCategory : `${serverUrl}/updateCategory`,
 
-    // allCategoriesApiUrl : 'http://localhost:3001/data',
-    // getAllRoles : 'http://localhost:3002/data',
+    getAllCategories : 'http://localhost:8000/data',
+    getAllRoles : 'http://localhost:8001/data',
     // updateCategory : 'http://localhost:3003/data',
 }
 
--- libs/shared/data-access/src/lib/queries/getAllCategories.ts
+++ libs/shared/data-access/src/lib/queries/getAllCategories.ts
@@ -1,10 +1,21 @@
-import { apiClient , apiUrlObject} from "@goal-tracker/data-access"
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
+import { apiClient } from '../api-client';
+import { apiUrlObject } from '../api-end-points';
 
-const getAllCategories = ():Promise<any> =>{
-    return apiClient.get(apiUrlObject.allCategoriesApiUrl);
-}
 
-export const useGetCategories = ()=>{
-    return useQuery({queryKey:['categories'],queryFn:getAllCategories});
-}   
+const getAllCategories = async (): Promise<any> => {
+    try {
+      const response = await apiClient.get(apiUrlObject.getAllCategories);
+      console.log('filterResponseC=>', response);
+      return response.data;
+    } catch (error) {
+      console.error('error', error);
+      throw error;
+    }
+  };
+
+  const QUERY_KEY = ['categories']
+
+export const useGetCategories = () => {
+  return useQuery({ queryKey: QUERY_KEY, queryFn: () => getAllCategories() });
+};
