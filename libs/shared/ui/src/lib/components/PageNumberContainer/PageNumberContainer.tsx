3bcf0805b6c694402ff0ea3fb81a6d2c64540655
Vishal Taral <vishaltaral09@gmail.com>
2024-01-29 17:26:19 +0530

    highlight to the current page

--- libs/shared/ui/src/lib/components/PageNumberContainer/PageNumberContainer.tsx
+++ libs/shared/ui/src/lib/components/PageNumberContainer/PageNumberContainer.tsx
@@ -2,23 +2,36 @@ import { useContext, useState } from 'react';
 import styles from './PageNumberContainer.module.scss';
 import AppContext from '../../contexts/AppContext';
 
-/* eslint-disable-next-line */
 export interface PageNumberContainerProps {
   totalPages: number;
 }
 
-export function PageNumberContainer({totalPages}) {
-  const context = useContext(AppContext)
-  const pageNumberHandler = (e) => {
-    console.log(e.target.innerText)
-    context.setPageNumber(e.target.innerText)
-  }
-  let pages=Array(totalPages).fill(0)
-  pages=pages.map((data,index)=>data=index+1)
+export function PageNumberContainer({ totalPages } : PageNumberContainerProps) {
+  const context = useContext(AppContext);
+  const [activePage, setActivePage] = useState(1);
+
+  const pageNumberHandler = (e : any) => {
+    const selectedPage = parseInt(e.target.innerText, 10);
+    console.log(selectedPage);
+    context.setPageNumber(selectedPage);
+    setActivePage(selectedPage);
+  };
+
+  let pages = Array(totalPages).fill(0);
+  pages = pages.map((data, index) => data = index + 1);
+
   return (
     <span className={styles.pageNumber_container}>
       {/* <div className={styles.pageNumber}>Prev</div> */}
-      {pages.map((data,index)=><div className={styles.pageNumber} onClick={pageNumberHandler} key={index}>{data}</div>)}
+      {pages.map((data, index) => (
+        <div
+          className={`${styles.pageNumber} ${activePage === data ? styles.activePage : ''}`}
+          onClick={pageNumberHandler}
+          key={index}
+        >
+          {data}
+        </div>
+      ))}
       {/* <div className={styles.pageNumber}>Next</div> */}
     </span>
   );
